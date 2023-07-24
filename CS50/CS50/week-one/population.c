52c60f238972b58c429846eea52914f74209f8ad
bsccode <bsccoding@gmail.com>
2023-07-24 17:18:08 +0800

    added old work from cs50

---
+++ CS50/CS50/week-one/population.c
@@ -0,0 +1,39 @@
+/*#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Global Variables
+    int n;
+    int t = 0;
+    int nt = 0;
+    int end;
+
+    // Get Starting and End Size "Do While" loops
+
+    do
+    {
+        n = get_int("Start size: ");
+    }
+    while (n < 9);
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < n);
+
+    //While loop with Mathematic loop, each iteration updates variable values.
+
+    while (n < end)
+    {
+        int born = n / 3;
+        int death = n / 4;
+        int growth = born - death;
+        n = n + growth;
+        t++;
+    }
+
+    //Final Print Statement of Years Taken
+
+    printf("Years: %i\n", t);
+}*/
